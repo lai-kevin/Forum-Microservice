025ee1f5a62b80e32137c13792d047b4f08497ea
Kevin Lai <kevinlaisoftware@gmail.com>
2023-12-03 19:26:38 -0500

    feat: login page

--- client/src/components/pages/login-page.jsx
+++ client/src/components/pages/login-page.jsx
@@ -1,34 +1,102 @@
 import { useEffect, useState } from "react";
-import { Card, Box, Typography, TextField, Button } from "@mui/material";
-import { blue } from "@mui/material/colors";
+import { Card, Box, Typography, TextField, Button, Alert } from "@mui/material";
 import Image from "../../images/collage-with-statue-meadow.jpg";
+import axios from "axios";
 
 const LoginPage = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [credentialsValid, setCredentialsValid] = useState(true);
 
-    const handleUsernameChange = (event) => {
-        setUsername(event.target.value);
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = () => {
+    setCredentialsValid(true);
+    let data = JSON.stringify({
+      email: email,
+      password: password,
+    });
+
+    let config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: "http://localhost:8000/api/users/login",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: data,
     };
 
-    const handlePasswordChange = (event) => {
-        setPassword(event.target.value);
-    };
+    axios
+      .request(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+        window.location.href = "/fakestackoverflow";
+      })
+      .catch((error) => {
+        setCredentialsValid(false);
+        console.log(error);
+      });
+  };
 
-    const handleLogin = () => {
-        // Perform login logic here
-    };
-
-    return (
-        <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", backgroundImage: `url(${Image})`, backgroundSize: "cover",}}>
-            <Card sx={{ display: "flex", flexDirection: "column", width: 300, padding: 5, borderRadius: 10, backdropFilter: "blur(10px)", backgroundColor: "rgba(255, 255, 255, 0.5)"}}>
-                <Typography variant="h3" color="white" textAlign="center">Login</Typography>
-                <TextField id="outlined-basic" label="Username" variant="outlined" required="true" value={username} onChange={handleUsernameChange} sx={{ margin: "10px 0" }} />
-                <TextField id="outlined-basic" label="Password" variant="outlined" required="true" value={password} onChange={handlePasswordChange} sx={{ margin: "10px 0" }} />
-                <Button variant="contained" color="primary" onClick={handleLogin}>Login</Button>
-            </Card>
-        </Box>
-    );
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundImage: `url(${Image})`,
+        backgroundSize: "cover",
+      }}
+    >
+      <Card
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          width: 300,
+          padding: 5,
+          borderRadius: 10,
+          backdropFilter: "blur(10px)",
+          backgroundColor: "rgba(255, 255, 255, 0.5)",
+        }}
+      >
+        <Typography variant="h3" color="white" textAlign="center">
+          Login
+        </Typography>
+        <TextField
+          id="outlined-basic"
+          label="E-mail"
+          variant="outlined"
+          required={true}
+          value={email}
+          onChange={handleEmailChange}
+          sx={{ margin: "10px 0" }}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Password"
+          variant="outlined"
+          required={true}
+          value={password}
+          onChange={handlePasswordChange}
+          sx={{ margin: "10px 0" }}
+        />
+        {!credentialsValid && (
+          <Alert severity="error">Invalid credentials</Alert>
+        )}
+        <Button variant="contained" color="primary" onClick={handleLogin}>
+          Login
+        </Button>
+      </Card>
+    </Box>
+  );
 };
 
 export default LoginPage;
